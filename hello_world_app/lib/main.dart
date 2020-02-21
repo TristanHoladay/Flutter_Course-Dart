903c530a26e1648e216a2cb304b3e268348ec501
TristanHoladay <tholaday777@gmail.com>
2020-02-21 09:01:47 -0800

    adding scaffold, appbar, icons, and buttons to hello world

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'ui/home.dart';
 
-void main() => runApp(Home());
+//void main() => runApp(ScaffoldExample());
+
+void main() => runApp(new MaterialApp(
+  home: ScaffoldExample(),
+));
 
 
 
--- hello_world_app/lib/ui/home.dart
+++ hello_world_app/lib/ui/home.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
 
+class ScaffoldExample extends StatelessWidget {
+  _tapButton() {
+    debugPrint("Tapped!");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Scaffold"),
+            centerTitle: true,
+            backgroundColor: Colors.amberAccent.shade700,
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.email),
+                  onPressed: () => debugPrint("Email Tapped!")),
+              IconButton(icon: Icon(Icons.access_alarms), onPressed: _tapButton)
+            ]),
+        backgroundColor: Colors.orangeAccent.shade400,
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              InkWell(
+                child: FloatingActionButton(
+                  child: Text("Hello World!"),
+                  backgroundColor: Colors.amberAccent,
+                  //foregroundColor: Colors.amberAccent,
+                  hoverColor: Colors.blueAccent,
+                  onPressed: () => debugPrint("Gabrielle Clicked Me!!!!!"),
+                ))
+            ]
+          )
+        ));
+  }
+}
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material (
+    return Material(
       color: Colors.deepOrange,
       child: Center(
-          child: Text("Hello World!", textDirection: TextDirection.ltr,
+          child: Text("Hello World!",
+              textDirection: TextDirection.ltr,
               style: TextStyle(
                   fontWeight: FontWeight.w500,
                   fontSize: 23.4,
-                  fontStyle: FontStyle.italic
-              ))),
+                  fontStyle: FontStyle.italic))),
     );
   }
 }
