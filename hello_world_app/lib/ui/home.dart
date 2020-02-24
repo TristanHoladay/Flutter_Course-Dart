b4f7b31b019f5de668af5c76e7806cacc9f25df1
TristanHoladay <tholaday777@gmail.com>
2020-02-24 02:23:58 -0800

    using GestureDetector vs Inkwell

--- hello_world_app/lib/ui/home.dart
+++ hello_world_app/lib/ui/home.dart
@@ -24,20 +24,43 @@ class ScaffoldExample extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              CustomButton()
 
-              InkWell(
-                child: FloatingActionButton(
-                  child: Text("Hello World!"),
-                  backgroundColor: Colors.amberAccent,
-                  //foregroundColor: Colors.amberAccent,
-                  hoverColor: Colors.blueAccent,
-                  onPressed: () => debugPrint("Gabrielle Clicked Me!!!!!"),
-                ))
+//              InkWell(
+//                child: FloatingActionButton(
+//                  child: Text("Hello World!"),
+//                  backgroundColor: Colors.amberAccent,
+//                  //foregroundColor: Colors.amberAccent,
+//                  hoverColor: Colors.blueAccent,
+//                  onPressed: () => debugPrint("Gabrielle Clicked Me!!!!!"),
+//                ))
             ]
           )
         ));
   }
 }
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackBar = SnackBar(content: Text("Hello World Again!"),);
+
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+     child: Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+        color: Colors.pinkAccent,
+        borderRadius: BorderRadius.circular(8.0),
+    ),
+       child: Text("Button"),
+    ),
+    );
+  }
+}
+
 
 class Home extends StatelessWidget {
   @override
