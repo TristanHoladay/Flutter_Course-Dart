426d18d713dbce77c060b78405bdef7c421b999e
TristanHoladay <tholaday777@gmail.com>
2020-02-27 10:03:35 -0800

    tried making a few changes to bizcard app

--- bizcard_app/lib/ui/home.dart
+++ bizcard_app/lib/ui/home.dart
@@ -1,3 +1,6 @@
+//import 'dart:html';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class BizCard extends StatelessWidget {
@@ -5,7 +8,13 @@ class BizCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("BizCard"),
+        title: Text("BizCard", textAlign: TextAlign.center),
+        centerTitle: true,
+        actions: <Widget>[
+          Icon(Icons.airline_seat_flat_angled,
+            color: Colors.deepPurpleAccent,
+            size: 30.0,)
+        ],
 
       ),
       body: Container(
@@ -28,7 +37,7 @@ class BizCard extends StatelessWidget {
       margin: EdgeInsets.all(50.0),
       decoration: BoxDecoration(
         color: Colors.pinkAccent,
-        borderRadius: BorderRadius.circular(4.5)
+        borderRadius: BorderRadius.circular(10)
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -37,12 +46,12 @@ class BizCard extends StatelessWidget {
           style: TextStyle(fontSize: 20.9,
           color: Colors.white,
           fontWeight: FontWeight.w500)),
-          Text("Build Apps with Tristan"),
+          Text("Build Apps with Tristan", style: TextStyle(fontSize: 20.0),),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Icon(Icons.person_outline),
-              Text("T: @buildappswithme")
+              Text("T: @buildappswithme", style: TextStyle(fontSize: 18.0),)
             ],
           )
         ],
